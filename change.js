515e59225033d874ef7093ec43f9075fc4d0db15
ccm7676 <50554024+ccm7676@users.noreply.github.com>
2020-02-14 23:19:16 +0100

    Update change.js

--- change.js
+++ change.js
@@ -2,3 +2,8 @@ function change() {
   var img = document.getElementById("img");
   img.src = "https://i1.sndcdn.com/avatars-000339084123-nag0p1-t500x500.jpg";
 }
+function submit() {
+  var img = document.getElementById("img");
+  var inp = document.getElementByIs("box");
+  img.src = inp.innerHTML;
+}
